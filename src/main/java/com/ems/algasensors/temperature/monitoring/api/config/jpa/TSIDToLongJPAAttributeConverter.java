5d620ad6dfc7c3d44bbffcc1d8e53c0fab071368
Kleber Souza <kleber.souza@any.mail.com.br>
2025-10-14 07:33:56 -0300

    Implemented jackson serializer configuration; Implemented JPA converter; Added SensorId model.

---
+++ src/main/java/com/ems/algasensors/temperature/monitoring/api/config/jpa/TSIDToLongJPAAttributeConverter.java
@@ -0,0 +1,21 @@
+package com.ems.algasensors.temperature.monitoring.api.config.jpa;
+
+import io.hypersistence.tsid.TSID;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+/*In all places that is possible to convert TSID attribute to Long and vice versa, this converter will be used.*/
+@Converter(autoApply = true)
+public class TSIDToLongJPAAttributeConverter implements AttributeConverter<TSID, Long> {
+
+    @Override
+    public Long convertToDatabaseColumn(TSID attribute) {
+        return attribute.toLong();
+    }
+
+    @Override
+    public TSID convertToEntityAttribute(Long dbData) {
+        return TSID.from(dbData);
+    }
+
+}
